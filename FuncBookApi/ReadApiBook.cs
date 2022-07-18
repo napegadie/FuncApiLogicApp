a4b6468a88669e9663376b8d3342cfc8a944d67b
Napegadie Kone <nakone@microsoft.com>
2022-07-18 09:30:08 -0500

    Add project files.

---
+++ FuncBookApi/ReadApiBook.cs
@@ -0,0 +1,59 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace FuncBookApi
+{
+    public static class ReadApiBook
+    {
+        [FunctionName("ReadApiBook")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+
+        List<Book> _lst = new List<Book>();
+
+            // Get the connection string from app settings and use it to create a connection.
+            var str = Environment.GetEnvironmentVariable("SQLAZURECONNSTR_SQLConnectionString");
+
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+                var text = "SELECT BookId,BookTitle,BookLocationName,BookAuthor,BookGenre,DatePublished from Book";
+
+                using (SqlCommand cmd = new SqlCommand(text, conn))
+                {
+                    using (SqlDataReader _reader = cmd.ExecuteReader())
+                    {
+                        while (_reader.Read())
+                        {
+                            Book _book = new Book()
+                            {
+                                BookId = _reader.GetInt32(0),
+                                BookTitle = _reader.GetString(1),
+                                BookLocationName = _reader.GetString(1),
+                                BookAuthor = _reader.GetString(1),
+                                BookGenre = _reader.GetString(1),
+                                DatePublished = _reader.GetDateTime(2)
+                            };
+
+                            _lst.Add(_book);
+                        }
+                    }
+
+                }
+            }
+
+            return new OkObjectResult(_lst);
+        }
+    }
+}
---
+++ FuncBookApi/book.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncBookApi
+{
+    public class Book
+    {
+        [Key]
+        [Required]
+        public int BookId { get; set; }
+        public string BookTitle { get; set; }
+
+        public string BookLocationName { get; set; }
+        public string BookLocationPath { get; set; }
+        public string BookAuthor { get; set; }
+        public string BookGenre { get; set; }
+        public DateTime? DatePublished { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+    }
+}
